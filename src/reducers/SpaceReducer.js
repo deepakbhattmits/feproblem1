61c641c5abd32b42d58eaede505164bfef64c9d8
deepakbhattmits <deepakbhattmits@gmail.com>
2021-09-19 12:52:35 +0530

    Update SpaceReducer.js

--- src/reducers/SpaceReducer.js
+++ src/reducers/SpaceReducer.js
@@ -4,43 +4,46 @@ import {
             GET_TOKEN,
             GET_TIME
         } from '../actions/types';
-export default (    state   =  {
-                                    vehicles   :   {}, 
-                                    planets    :   {},
-                                    token      :    {},
-                                    time       :    []
-                                },
-                    action 
-                ) => {
-                switch( action.type) {
-                    case FETCH_VEHICLES :
-                    return Object.assign(
-                                            {}, 
-                                            state, 
-                                            {   
-                                                vehicles:  action.payload
-                                            }
-                                        );
+const spaceReducer = (state = {
+    vehicles: {},
+    planets: {},
+    token: {},
+    time: []
+},
+    action
+) =>
+{
+    switch (action.type)
+    {
+        case FETCH_VEHICLES:
+            return Object.assign(
+                {},
+                state,
+                {
+                    vehicles: action.payload
+                }
+            );
                     
-                    case FETCH_PLANETS :
-                    return Object.assign(   {}, 
-                                            state, 
-                                            { 
-                                                planets: action.payload 
-                                            }
-                                        )
-                    case GET_TOKEN :
-                    return Object.assign (
-                            {},
-                            state,
-                            { 
-                                token: action.payload 
-                            }
-                    )
-                    case GET_TIME :
+        case FETCH_PLANETS:
+            return Object.assign({},
+                state,
+                {
+                    planets: action.payload
+                }
+            );
+        case GET_TOKEN:
+            return Object.assign(
+                {},
+                state,
+                {
+                    token: action.payload
+                }
+            );
+        case GET_TIME:
                          
-                    return Object.assign({},state,{time: action.payload} );
-                    default :
-                    return state;
-            }
-}
+            return Object.assign({}, state, {time: action.payload});
+        default:
+            return state;
+    }
+};
+export default spaceReducer;
